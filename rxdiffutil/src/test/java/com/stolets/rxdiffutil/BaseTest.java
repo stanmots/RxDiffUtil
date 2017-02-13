a61bc5c39c354630f2fe1a8998afc8b41af89a21
Stan Mots <stuffokator@gmail.com>
2017-02-13 14:15:46 +0200

    Add BaseTest

---
+++ rxdiffutil/src/test/java/com/stolets/rxdiffutil/BaseTest.java
@@ -0,0 +1,14 @@
+package com.stolets.rxdiffutil;
+
+import org.junit.Before;
+import org.mockito.MockitoAnnotations;
+
+abstract class BaseTest {
+    /**
+     * The @Before methods of the base class will be run before those of the subclasses.
+     */
+    @Before
+    public void setupBaseBefore() {
+        MockitoAnnotations.initMocks(this);
+    }
+}
