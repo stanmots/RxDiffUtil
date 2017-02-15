82745032790463d13eebe9d833b120d02616a620
Stan Mots <stuffokator@gmail.com>
2017-02-15 13:26:00 +0200

    Add Constants

---
+++ rxdiffutil/src/main/java/com/stolets/rxdiffutil/internal/Constants.java
@@ -0,0 +1,18 @@
+package com.stolets.rxdiffutil.internal;
+
+import com.stolets.rxdiffutil.diffrequest.DiffRequest;
+
+/**
+ * Global project constants.
+ */
+public final class Constants {
+    /**
+     * The default tag used to identify {@link DiffRequest}.
+     */
+    public static final String DIFF_REQUEST_DEFAULT_TAG = "DIFF_REQUEST_DEFAULT_TAG";
+
+    /**
+     * The tag which identifies the retained fragment used to listen to the given {@link android.app.Activity} lifecycle.
+     */
+    public static final String RETAINED_FRAGMENT_TAG = "com.stolets.rxdiffutil.RETAINED_FRAGMENT_TAG";
+}
