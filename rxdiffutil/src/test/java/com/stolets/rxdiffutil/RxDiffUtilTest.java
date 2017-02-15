51d02ab43b576c70c3ceb0e8baa116588cf23c48
Stan Mots <stuffokator@gmail.com>
2017-02-15 13:29:16 +0200

    Add RxDiffUtilTest unit tests

---
+++ rxdiffutil/src/test/java/com/stolets/rxdiffutil/RxDiffUtilTest.java
@@ -0,0 +1,31 @@
+package com.stolets.rxdiffutil;
+
+import android.app.Activity;
+import android.support.v7.util.DiffUtil;
+
+import com.stolets.rxdiffutil.diffrequest.DiffRequestBuilder;
+
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class RxDiffUtilTest extends BaseTest {
+    @Mock
+    Activity mActivity;
+
+    @Mock
+    DiffUtil.Callback mCallback;
+
+    @Test
+    public void builder_ReturnsNotNullDiffRequestBuilder() {
+        // Given an activity and a callback
+
+        // When
+        final DiffRequestBuilder builder = RxDiffUtil.builder(mActivity, mCallback);
+
+        // Then
+        assertThat(builder, notNullValue());
+    }
+}
