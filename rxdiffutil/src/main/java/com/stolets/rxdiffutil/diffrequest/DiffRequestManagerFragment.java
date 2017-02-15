2e5dea9bc6c2e01972805b1bb6d8770984e3d6df
Stan Mots <stuffokator@gmail.com>
2017-02-15 13:32:30 +0200

    Add DIffRequestManagerFragment

---
+++ rxdiffutil/src/main/java/com/stolets/rxdiffutil/diffrequest/DiffRequestManagerFragment.java
@@ -0,0 +1,39 @@
+package com.stolets.rxdiffutil.diffrequest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.stolets.rxdiffutil.util.DaggerUtils;
+
+import javax.inject.Inject;
+
+/**
+ * The view-less retained fragment that is used to hold {@link DiffRequestManager}.
+ */
+public class DiffRequestManagerFragment extends Fragment {
+    @Inject
+    DiffRequestManager mDiffRequestManager;
+
+    public static DiffRequestManagerFragment newInstance() {
+        return new DiffRequestManagerFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+
+        ((DiffRequestSubcomponent.Builder) DaggerUtils
+                .subcomponentBuilderFor(DiffRequestSubcomponent.class))
+                .build()
+                .inject(this);
+    }
+
+    /**
+     * @return {@link DiffRequestManager} instance.
+     */
+    public DiffRequestManager getDiffRequestManager() {
+        return mDiffRequestManager;
+    }
+}
