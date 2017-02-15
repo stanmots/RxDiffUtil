5deb1e864e90faa0fdc6d4f4650ca287370e62fd
Stan Mots <stuffokator@gmail.com>
2017-02-15 13:17:09 +0200

    Add checkArgument precondition

--- rxdiffutil/src/main/java/com/stolets/rxdiffutil/internal/Preconditions.java
+++ rxdiffutil/src/main/java/com/stolets/rxdiffutil/internal/Preconditions.java
@@ -26,4 +26,18 @@ public final class Preconditions {
         }
         return reference;
     }
+
+    /**
+     * Ensures the truth of an expression involving one or more parameters to the calling method.
+     *
+     * @param expression a boolean expression
+     * @param errorMessage the exception message to use if the check fails; will be converted to a
+     *     string using {@link String#valueOf(Object)}
+     * @throws IllegalArgumentException if {@code expression} is false
+     */
+    public static void checkArgument(boolean expression, @Nullable Object errorMessage) {
+        if (!expression) {
+            throw new IllegalArgumentException(String.valueOf(errorMessage));
+        }
+    }
 }
