0da59489c4a697c6f9acf6316e078a479734c58e
Stan Mots <stuffokator@gmail.com>
2017-02-15 15:22:52 +0200

    Update unit tests

---
+++ rxdiffutil/src/test/java/com/stolets/rxdiffutil/TestAdapter.java
@@ -0,0 +1,35 @@
+package com.stolets.rxdiffutil;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+public class TestAdapter<T> extends RecyclerView.Adapter<TestAdapter.TestViewHolder> implements Updatable<T> {
+    @Override
+    public TestAdapter.TestViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new TestViewHolder(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(TestAdapter.TestViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public void update(List<? extends T> newData) {
+
+    }
+
+    static class TestViewHolder extends RecyclerView.ViewHolder {
+        TestViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
--- rxdiffutil/src/test/java/com/stolets/rxdiffutil/util/MockitoUtils.java
+++ rxdiffutil/src/test/java/com/stolets/rxdiffutil/util/MockitoUtils.java
@@ -2,16 +2,17 @@ package com.stolets.rxdiffutil.util;
 
 
 import com.stolets.rxdiffutil.DefaultDiffCallback;
+import com.stolets.rxdiffutil.TestAdapter;
 import com.stolets.rxdiffutil.TestModel;
 
 import java.util.Arrays;
 import java.util.List;
 
 public final class MockitoUtils {
-    public static DefaultDiffCallback<String, TestModel> getMockedDefaultDiffCallback() {
+    public static DefaultDiffCallback<String, TestModel, TestAdapter<TestModel>> getMockedDefaultDiffCallback() {
         final List<TestModel> mOldList = Arrays.asList(new TestModel("s1", "id1"), new TestModel("s2", "id2"));
         final List<TestModel> mNewList = Arrays.asList(new TestModel("s1", "id10"), new TestModel("s3", "id2"));
 
-        return new DefaultDiffCallback<>(mOldList, mNewList);
+        return new DefaultDiffCallback<>(mOldList, mNewList, new TestAdapter<TestModel>());
     }
 }
