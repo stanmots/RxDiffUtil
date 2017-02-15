0a136bf1d3e83844d2b8a3907745ba8f4703700a
Stan Mots <stuffokator@gmail.com>
2017-02-15 14:55:58 +0200

    Remove redundant recyclerview adapter from DiffRequest

--- rxdiffutil/src/main/java/com/stolets/rxdiffutil/diffrequest/DiffRequest.java
+++ rxdiffutil/src/main/java/com/stolets/rxdiffutil/diffrequest/DiffRequest.java
@@ -1,11 +1,7 @@
 package com.stolets.rxdiffutil.diffrequest;
 
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.util.DiffUtil;
-import android.support.v7.widget.RecyclerView;
-
-import java.lang.ref.WeakReference;
 
 import static com.stolets.rxdiffutil.internal.Preconditions.checkArgument;
 import static com.stolets.rxdiffutil.internal.Preconditions.checkNotNull;
@@ -18,8 +14,6 @@ public final class DiffRequest {
     private final DiffUtil.Callback mDiffCallback;
     @NonNull
     private final String mTag;
-    @Nullable
-    private WeakReference<RecyclerView.Adapter> mAdapterWeakRef;
     private boolean mDetectMoves;
 
     /**
@@ -57,28 +51,6 @@ public final class DiffRequest {
     }
 
     /**
-     * @return {@link android.support.v7.widget.RecyclerView.Adapter} or null it wasn't set.
-     */
-    @Nullable
-    public RecyclerView.Adapter getAdapter() {
-        return mAdapterWeakRef != null ? mAdapterWeakRef.get() : null;
-    }
-
-    /**
-     * Sets the optinal {@link android.support.v7.widget.RecyclerView.Adapter}.
-     * <p>
-     * Note: when the adapter is set it will be notified automatically about data changes.
-     * </p>
-     *
-     * @param adapter {@link android.support.v7.widget.RecyclerView.Adapter}.
-     */
-    @SuppressWarnings("WeakerAccess")
-    public void setAdapter(@NonNull final RecyclerView.Adapter adapter) {
-        checkNotNull(adapter, "adapter must not be null!");
-        mAdapterWeakRef = new WeakReference<>(adapter);
-    }
-
-    /**
      * @return True if DiffUtil should try to detect moved items, false otherwise.
      */
     public boolean isDetectingMoves() {
@@ -100,22 +72,15 @@ public final class DiffRequest {
 
         DiffRequest that = (DiffRequest) o;
 
-        final RecyclerView.Adapter adapter = mAdapterWeakRef != null ? mAdapterWeakRef.get() : null;
-        final RecyclerView.Adapter thatAdapter = that.mAdapterWeakRef != null ? that.mAdapterWeakRef.get() : null;
-
         return mDetectMoves == that.mDetectMoves &&
                 mTag.equals(that.getTag()) &&
-                adapter != null ? adapter.equals(thatAdapter) : thatAdapter == null &&
                 mDiffCallback.equals(that.mDiffCallback);
     }
 
     @Override
     public int hashCode() {
-        final RecyclerView.Adapter adapter = mAdapterWeakRef != null ? mAdapterWeakRef.get() : null;
-
         int result = mDiffCallback.hashCode();
         result = 31 * result + mTag.hashCode();
-        result = 31 * result + (adapter != null ? adapter.hashCode() : 0);
         result = 31 * result + (mDetectMoves ? 1 : 0);
         return result;
     }
