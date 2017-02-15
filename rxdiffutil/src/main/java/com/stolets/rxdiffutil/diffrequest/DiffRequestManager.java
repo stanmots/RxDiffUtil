85579590b61930797d7c0a5a8a950d2dedaa53f8
Stan Mots <stuffokator@gmail.com>
2017-02-15 13:30:47 +0200

    Add DiffRequestManager

---
+++ rxdiffutil/src/main/java/com/stolets/rxdiffutil/diffrequest/DiffRequestManager.java
@@ -0,0 +1,38 @@
+package com.stolets.rxdiffutil.diffrequest;
+
+import android.support.annotation.NonNull;
+
+import com.stolets.rxdiffutil.di.FragmentScope;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.stolets.rxdiffutil.internal.Preconditions.checkNotNull;
+
+/**
+ * Manages the diff requests lifecycle.
+ */
+@FragmentScope
+public final class DiffRequestManager {
+    @NonNull
+    private Map<String, DiffRequest> mPendingRequests = new HashMap<>();
+
+    /**
+     * Adds a new {@link DiffRequest} to the pending list.
+     * @param diffRequest A new {@link DiffRequest}.
+     *                    Note: if a request with the same tag is already exists it will be replaced.
+     */
+    public void addPendingRequest(@NonNull final DiffRequest diffRequest) {
+        checkNotNull(diffRequest, "diffRequest must not be null!");
+        mPendingRequests.put(diffRequest.getTag(), diffRequest);
+    }
+
+    /**
+     * @return A map holding all pending requests. Note: the returned map is unmodifiable.
+     */
+    @NonNull
+    public Map<String, DiffRequest> getPendingRequests() {
+        return Collections.unmodifiableMap(mPendingRequests);
+    }
+}
