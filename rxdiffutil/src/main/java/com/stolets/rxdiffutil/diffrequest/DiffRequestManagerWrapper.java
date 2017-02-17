ba4971920ad1e91eb7299cbf093eb36675c36d0d
Stan Mots <stuffokator@gmail.com>
2017-02-17 21:26:42 +0200

    Add calculate method to start diff calculations

--- rxdiffutil/src/main/java/com/stolets/rxdiffutil/diffrequest/DiffRequestManagerWrapper.java
+++ rxdiffutil/src/main/java/com/stolets/rxdiffutil/diffrequest/DiffRequestManagerWrapper.java
@@ -3,31 +3,56 @@ package com.stolets.rxdiffutil.diffrequest;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.stolets.rxdiffutil.RxDiffResult;
+
 import java.lang.ref.WeakReference;
+
+import io.reactivex.Single;
 
 import static com.stolets.rxdiffutil.internal.Preconditions.checkArgument;
 import static com.stolets.rxdiffutil.internal.Preconditions.checkNotNull;
 
 /**
  * This class is used as a wrapper around {@link DiffRequestManager} to prevent memory leaks.
+ * <p>
+ * Note: The underlying {@link DiffRequestManager} will be GCd when the activity passed to {@link DiffRequestBuilder} is finished completely (not just due a configuration change).
+ * </p>
  */
+@SuppressWarnings("WeakerAccess")
 public final class DiffRequestManagerWrapper {
-    @Nullable
-    private WeakReference<DiffRequestManager> mDiffRequestManager;
     @NonNull
-    private String mTag;
+    private final WeakReference<DiffRequestManager> mDiffRequestManagerWeakRef;
+    @NonNull
+    private final String mTag;
 
-    public DiffRequestManagerWrapper(@NonNull final String tag) {
+    /**
+     * Constructs a wrapper that holds a weak reference to {@link DiffRequestManager}.
+     * @param diffRequestManager {@link DiffRequestManager}.
+     * @param tag A tag that is passed to the underlying {@link DiffRequestManager} to handle the appropriate request.
+     */
+    public DiffRequestManagerWrapper(@NonNull final DiffRequestManager diffRequestManager, @NonNull final String tag) {
+        checkNotNull(diffRequestManager, "diffRequestManager must not be null!");
         checkNotNull(tag, "tag string must not be null!");
         checkArgument(!tag.isEmpty(), "tag string must not be empty!");
+
+        this.mDiffRequestManagerWeakRef = new WeakReference<>(diffRequestManager);
         this.mTag = tag;
     }
 
-    public void attachDiffRequestManager(@NonNull final DiffRequestManager diffRequestManager) {
-        checkNotNull(diffRequestManager, "diffRequestManager must not be null!");
-        this.mDiffRequestManager = new WeakReference<DiffRequestManager>(diffRequestManager);
+    /**
+     * Starts the difference calculation in accordance with parameters specified in {@link DiffRequestBuilder}.
+     *
+     * @return {@link Single} you can subscribe to or null if the activity has been finished. It also returns null if you have passed {@link com.stolets.rxdiffutil.DefaultDiffCallback} to the builder, in this case the result is handled internally automatically.
+     */
+    @Nullable
+    public Single<RxDiffResult> calculate() {
+        final DiffRequestManager diffRequestManager = mDiffRequestManagerWeakRef.get();
+        return diffRequestManager != null ? diffRequestManager.execute(mTag) : null;
     }
 
+    /**
+     * @return The tag you specified in {@link DiffRequestBuilder}.
+     */
     @NonNull
     public String getTag() {
         return mTag;
