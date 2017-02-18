df59943f6ccf0b8d778c16e63a8964a58a4c3457
Stan Mots <stuffokator@gmail.com>
2017-02-18 18:18:53 +0200

    Refactor RxDiffUtil entry point method

--- rxdiffutil/src/main/java/com/stolets/rxdiffutil/RxDiffUtil.java
+++ rxdiffutil/src/main/java/com/stolets/rxdiffutil/RxDiffUtil.java
@@ -1,6 +1,5 @@
 package com.stolets.rxdiffutil;
 
-import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.v7.util.DiffUtil;
 
@@ -17,16 +16,13 @@ public final class RxDiffUtil {
     /**
      * Creates the diff request builder that is used to configure request parameters.
      *
-     * @param activity     An {@link Activity} the lifecycle of the request is bound to.
-     *                     Note: When the activity is finished the request is cancelled automatically.
      * @param diffCallback A concrete implementation of {@link DiffUtil.Callback}.
      * @return {@link DiffRequestBuilder}.
      */
     @NonNull
-    public static DiffRequestBuilder builder(@NonNull final Activity activity, @NonNull final DiffUtil.Callback diffCallback) {
-        checkNotNull(activity, "activity must not be null!");
+    public static DiffRequestBuilder with(@NonNull final DiffUtil.Callback diffCallback) {
         checkNotNull(diffCallback, "diffCallback must not be null!");
 
-        return new DiffRequestBuilder(activity, diffCallback);
+        return new DiffRequestBuilder(diffCallback);
     }
 }
