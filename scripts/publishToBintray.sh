022cac5680c92ddcef8e39d48b2bb542c298f9a0
Stan Mots <stuffokator@gmail.com>
2017-02-21 01:42:46 +0200

    Fix comment string

--- scripts/publishToBintray.sh
+++ scripts/publishToBintray.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script will publish the project to Bintray via Travis C.
+# This script will publish the project to Bintray via Travis CI.
 
 if [[ "$TRAVIS_TEST_RESULT" -eq 0 ]] && [[ -n "$TRAVIS_TAG" ]] && [[ "$TRAVIS_BRANCH" == 'master' && "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
     echo "Pushing build to Bintray"
