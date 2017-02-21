b16c7b5b062d45a50e4e4f8effc5e7a8a882bd94
Stan Mots <stuffokator@gmail.com>
2017-02-21 21:08:29 +0200

    Do not check TRAVIS_BRANCH because of the bug in Travis

--- scripts/publishToBintray.sh
+++ scripts/publishToBintray.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # This script will publish the project to Bintray via Travis CI.
 
-echo "Checking whether the artifact should be published to Bintray"
+echo "Checking whether the artifacts should be published to Bintray"
 echo "TRAVIS_TEST_RESULT is $TRAVIS_TEST_RESULT"
 echo "TRAVIS_TAG is $TRAVIS_TAG"
 echo "TRAVIS_BRANCH is $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST is $TRAVIS_PULL_REQUEST"
 
-if [[ "$TRAVIS_TEST_RESULT" -eq 0 ]] && [[ -n "$TRAVIS_TAG" ]] && [[ "$TRAVIS_BRANCH" == 'master' && "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
+# Do not check TRAVIS_BRANCH because of the bug in Travis
+# see https://github.com/travis-ci/travis-ci/issues/4745
+if [[ "$TRAVIS_TEST_RESULT" -eq 0 ]] && [[ -n "$TRAVIS_TAG" ]] && [[ "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
     echo "Pushing to Bintray"
     ./gradlew bintrayUpload
 else
