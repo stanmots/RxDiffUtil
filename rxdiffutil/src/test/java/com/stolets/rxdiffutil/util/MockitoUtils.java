2a48cab378d807788a23855ca2220669599b8418
Stan Mots <stuffokator@gmail.com>
2017-02-21 17:28:45 +0200

    Add getSubbedDiffRequestManager util method

--- rxdiffutil/src/test/java/com/stolets/rxdiffutil/util/MockitoUtils.java
+++ rxdiffutil/src/test/java/com/stolets/rxdiffutil/util/MockitoUtils.java
@@ -28,15 +28,35 @@ package com.stolets.rxdiffutil.util;
 import com.stolets.rxdiffutil.DefaultDiffCallback;
 import com.stolets.rxdiffutil.TestAdapter;
 import com.stolets.rxdiffutil.TestModel;
+import com.stolets.rxdiffutil.diffrequest.DiffRequest;
+import com.stolets.rxdiffutil.diffrequest.DiffRequestManager;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 
 public final class MockitoUtils {
+    @SuppressWarnings("WeakerAccess")
+    public static final String TEST_TAG = "TEST_TAG";
+
     public static DefaultDiffCallback<String, TestModel, TestAdapter<TestModel>> getStubbedDefaultDiffCallback() {
         final List<TestModel> mOldList = Arrays.asList(new TestModel("s1", "id1"), new TestModel("s2", "id2"));
         final List<TestModel> mNewList = Arrays.asList(new TestModel("s1", "id10"), new TestModel("s3", "id2"));
 
         return new DefaultDiffCallback<>(mOldList, mNewList, new TestAdapter<TestModel>());
+    }
+
+    public static DiffRequestManager getStubbedDiffRequestManager() {
+        final DiffRequest diffRequest = new DiffRequest(true, TEST_TAG, MockitoUtils.getStubbedDefaultDiffCallback());
+        final Map<String, DiffRequest> requestMap = new HashMap<>();
+        requestMap.put(TEST_TAG, diffRequest);
+
+
+        return new DiffRequestManager(requestMap,
+                new HashMap<String, Disposable>(), new CompositeDisposable());
     }
 }
